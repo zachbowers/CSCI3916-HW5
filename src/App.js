71614048ef67db45d2a93bb4d5fbfc0de9d0ca08
zach42o1 <48109689+zach42o1@users.noreply.github.com>
2020-03-02 17:30:10 -0700

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MovieHeader from './components/movieheader';
 import Authentication from './components/authentication';
